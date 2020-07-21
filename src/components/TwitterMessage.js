e2570de8f3ca9e197bf4ce3976766e02249e1b81
sdcro <sdcrouse91@gmail.com>
2020-07-21 16:25:28 -0700

    Add missing <input> attributes

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -20,7 +20,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" onChange={this.handleChange} value={this.state.message}/>
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
         {charNumber}
       </div>
     );
